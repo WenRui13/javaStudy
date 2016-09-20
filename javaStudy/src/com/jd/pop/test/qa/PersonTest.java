80b8e6913e7fcb2291b62558519d858335f6fd99
bjyfxuxiaojun <yfxuxiaojun@jd.com>
2016-09-20 23:18:05 +0800

    测试提交代码

---
+++ javaStudy/src/com/jd/pop/test/qa/PersonTest.java
@@ -0,0 +1,10 @@
+package com.jd.pop.test.qa;
+
+import org.testng.annotations.Test;
+
+public class PersonTest {
+  @Test
+  public void test1() {
+	  System.out.println("my first test case.");
+  }
+}
